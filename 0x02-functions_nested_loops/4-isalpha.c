fe32427e485654fa41108b17d544a89c67074995
brownchristian64 <brownchristian64@gmail.com>
2023-06-21 19:30:31 -0700

    sign

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _isalph - checks for alphabetic character
+ * _isalpha - checks for alphabetic character
  * @c: The character to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
 
 int _isalpha(int c)
+
 {
 
 if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
@@ -14,4 +15,5 @@ if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
 return (1);
 }
 return (0);
+
 }
